b32c3034b3f7d02fed7d22bf49c72a6c7d9b1488
irena mali <irena.mali@speedandfunction.com>
2021-11-27 11:47:34 +0200

    done

--- js/module2/functionalScope.js
+++ js/module2/functionalScope.js
@@ -24,3 +24,22 @@
 //   console.log("3rd",value); 
 // }
 
+
+const findLongestWord = function (string) {
+    const stringArr = string.split(" ");
+    let longestWord = '';
+    for (let item of stringArr) {
+        if (item.length > longestWord.length) {
+            longestWord =  item
+        } 
+    return longestWord
+};
+
+/*
+ * Виклич функції для перевірки працездатності твоєї реалізації.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazgggggggggy dog')); // 'jumped'
+
+console.log(findLongestWord('Google dgggggggggggo a roll')); // 'Google'
+
+console.log(findLongestWord('May the force bgggggggggggge with you')); // 'force'
---
+++ js/module3/object.js
@@ -0,0 +1,192 @@
+// const resortHotel = {}
+// console.log(resortHotel)
+
+// const hotel = {
+//     name: 'Resort Hotel',
+//     stars: 5,
+//     isBudget: false,
+//     listofRestrong: ['Main', "Beach", "Night Club"],
+//     management: {
+//         chief: "Golovach Stanislav",
+//         ceo: " Mr Cooper Bill",
+//         accountantdep: {mainAcc: "Glensta Helga"}
+//     }
+
+// };
+// console.log(hotel.name)
+// hotel.name = "Best Western"
+// console.log(hotel["name"])
+
+// console.log(hotel['stars'])
+// console.log(hotel.management)
+// console.log(hotel.management.ceo)
+// console.log(hotel.listofRestrong)
+// console.log(hotel.listofRestrong[0])
+// console.log(hotel.isRenovated = false)
+// console.log(hotel)
+// console.log(hotel.address = "Lviv")
+// console.log(hotel)
+
+
+// // ===========SHORTCAT==============
+// let name = 'Resort Hotel';
+// let stars = 5;
+
+// const myHotel = {
+//     name,
+//     stars,
+//     capacity: 100
+// }
+// console.log(myHotel)
+
+
+// const es5hotel = {
+//   name: name,
+//   stars: stars,
+//   capacity: 100,
+// };
+
+
+// console.log(es5hotel);
+
+
+// ============================================
+
+// const i = "greating"
+// const b = () => "age"
+// console.log(b())
+
+// const newObject = {
+//     [i]: "Hi",
+//     [b()] : 2
+
+// }
+// console.log(newObject)
+
+
+
+
+// ============METHODS=================
+
+// const train = {
+//     name: "Intercity",
+//     time: 3,
+//     getSpeed(distance){ return distance / this.time},
+// }
+// // train.time = 7
+// console.log(train.getSpeed(600))
+
+
+
+// const train = {
+//     name: "Interciti",
+//     distance: 600,
+//     time: 6,
+//     getSpeed() { return this.distance / this.time }
+// }
+
+// console.log(train.getSpeed())
+// train.time = 4;
+// console.log(train.getSpeed())
+
+// const flight = {
+//     name: "Antonov70",
+//     averageSpeed: 400,
+//     getDistance(time) { return time * this.averageSpeed }
+// };
+// console.log(flight.getDistance(5))
+
+// ============FOR IN================
+
+// const numbers = [1, 2, 3]
+// for (let item of numbers) {
+//     console.log(item)
+// }
+
+
+// const myHotel = {
+//     name: "Resort Hotel",
+//     capacity: 200,
+//     owner: "Mr Horoof",
+//     isRenovated: false
+// }
+// console.log(myHotel.name)
+
+
+// for (let key in myHotel) {
+//     console.log(key);
+//     console.log(myHotel[key])
+// }
+
+
+// for (let item in myHotel) {
+//     console.log(item)
+//     console.log(myHotel[item])
+// }
+
+
+// =========OBJECT======================
+
+// Object.keys(obj)
+// Object.values(obj)
+// Object.entries(obj)
+
+// const book = {
+//     name: "Geralt of Rivia",
+//     role: 'wizard',
+//     weapon: "sword",
+//     author: 'Andrzej Sapkowski',
+//     isFilming: true
+// }
+
+// const bookKeys = Object.keys(book)
+// // console.log(bookKeys)
+
+// const bookValues = Object.values(book)
+// // console.log(bookValues)
+
+// const bookEntries = Object.entries(book)
+// console.log(bookEntries)
+
+// console.log(bookKeys);
+// console.log(bookKeys[1])
+
+// console.log(bookValues)
+// console.log(bookValues[0])
+
+// book['name']
+// book["role"]
+
+// for (let element of bookKeys) {
+//     console.log(element, book[element])
+// }
+
+// for (let item of bookEntries) {
+//     const key = item[0]
+//     const value = item[1]
+//     console.log(`${key}: ${value}`)
+// }
+
+const salaryList = {
+    "Mongo": 100,
+    "Poly": 200,
+    "Deny": 150,
+    "Yen": 400
+}
+
+
+const values = Object.values(salaryList)
+console.log(values)
+
+let sum = 0;
+for (let value of values) {
+    sum += value
+    // values.length
+
+}
+console.log(sum/values.length)
+
+// // ==========SPREAD REST================
+
+// console.log(Math.min(values))
+// console.log(Math.min(...values))
